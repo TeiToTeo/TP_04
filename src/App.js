9e50169e8a915cb6daa11837cffc8067ff0cfe21
TeiToTeo <teo.e.navarro@gmail.com>
2023-05-10 08:26:36 -0300

    Algo anda!!!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Cita from '../components/cita'
+import Cita from './components/Cita'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Cita/>
     </div>
 // No se no anda y no entiendo   <Cita></Cita>
   );
--- src/components/Cita.jsx
+++ src/components/Cita.jsx
@@ -1,5 +1,13 @@
+
+
 const Cita = (props) => {
-    <div>
-        <h2>La cita:{props.num}</h2>
-    </div>
+    return (<div class="cita">
+              <p>Mascota: <span>Blacky</span></p>
+              <p>Dueño: <span>Jere</span></p>
+              <p>Fecha: <span>2023-05-08</span></p>
+              <p>Hora: <span>16:15</span></p>
+              <p>Sintomas: <span>No está comiendo</span></p><button class="button elimnar u-full-width">Eliminar ×</button>
+            </div>
+            )
 }
+export default Cita;
