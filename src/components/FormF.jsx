b431884b41206305db90730f3b54a3271c7f7561
TeiToTeo <teo.e.navarro@gmail.com>
2023-05-22 10:36:29 -0300

    asdasdas

--- src/components/FormF.jsx
+++ src/components/FormF.jsx
@@ -11,6 +11,18 @@ const Soplon = () => {
     <>
       <input type="text" onChange={handleChange} />
       <span>{texto}</span>
+      poner asi todo
+      <Formulario name1="Nombre Mascota" />
+      <input type="text" class="u-full-width" placeholder="Nombre Mascota" value="" />
+      <Formulario name1="Nombre Dueño" />
+      <input type="text" class="u-full-width" placeholder="Nombre Dueño" value="" />
+      <Formulario name1="Fecha" />
+      <input type="date" name="fecha" class="u-full-width" value="" />
+      <Formulario name1="Hora" />
+      <input type="time" name="hora" class="u-full-width" value="" />
+      <Formulario name1="Sintomas" />
+      <textarea name="sintomas" class="u-full-width" />
+      <button type="submit" class="u-full-width button-primary">Agregar Cita</button>
     </>
   );
 };
